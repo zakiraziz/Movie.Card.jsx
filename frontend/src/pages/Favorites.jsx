a76f801befc72e10a040d0f57bc3256b834289a9
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-05-02 19:53:19 +0500

    Update Favorites.jsx

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,9 +1,29 @@
-import "../css/Favorite.css";
-function Favorite() {
-    return <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Star adding movies to your favorites and they will apear hear</p>
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
+  );
 }
 
-export default Favorite
+export default Favorites;
