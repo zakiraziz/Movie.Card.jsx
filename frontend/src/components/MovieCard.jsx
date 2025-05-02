d13cf7159a1c92cb5674f6050560b9bdf09b6864
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-05-02 19:52:40 +0500

    Update MovieCard.jsx

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,23 +1,28 @@
-import "../css/MovieCard.css";
+import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
 function MovieCard({movie}) {
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-    function onFavoriteClick() {
-        alert(clicked)
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
-                <button className="favorite-btn" onClick={anFavoriteClck}>
-                ❤️
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
                 </button>
             </div>
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
         </div>
     </div>
 }
